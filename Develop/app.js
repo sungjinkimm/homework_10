0604f259b12681c5ec18b009e86c8f6f172694b7
SungJin Kim <sungjinkimm@gmail.com>
2020-04-30 09:56:19 -0400

    fixed prompt/ cleaned up code

--- Develop/app.js
+++ Develop/app.js
@@ -2,7 +2,6 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Employee = require("./lib/Employee");
-
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -16,23 +15,41 @@ const render = require("./lib/htmlRenderer");
 // This will be an array of all team member objects created
 const teamMembers = [];
 
-// This will be an array of the id values created for each object so there are no duplicates
-const idArray = [];
+function createTeam() {
+  inquirer
+    .prompt([
+      // asking which type of team member should be created with a list of choices
+      {
+        type: "list",
+        message: "Which type of team member do you want to create?: ",
+        name: "role",
+        choices: ["Manager", "Engineer", "Intern", "Done"],
+      },
+    ])
+    .then((userChoice) => {
+      // Based on which choice they make, call the correct function to ask more questions.
+      // If no choice is made, then go to the rendering function.
+      if (userChoice.role === "Manager"){
+        createManager();
+      } else if (userChoice.role === "Engineer") {
+        createEngineer();
+      } else if (userChoice.role === "Intern") {
+        createIntern();
+      } else if(userChoice.role === "Done"){
+        renderHtmlPage()
+      }
+    });
+}
 
 // This function generates all the questions for creating the manager.
 function createManager() {
   console.log("Please build your team");
   inquirer
     .prompt([
-      {
-        type: "list",
-        message: "Select your role",
-        name: "role",
-        choices: ["Manager", "Engineer", "Intern"],
-      },
+      
       {
         type: "input",
-        message: "What is your manager's name?",
+        message: "What is the manager's name?",
         name: "name",
         // Note how the validate function works
         validate: (answer) => {
@@ -44,7 +61,7 @@ function createManager() {
       },
       {
         type: "input",
-        message: "What is your manager's E-Mail?",
+        message: "What is the manager's E-Mail?",
         name: "email",
 
         validate: (answer) => {
@@ -56,7 +73,7 @@ function createManager() {
       },
       {
         type: "input",
-        message: "What is your manager's ID?",
+        message: "What is the manager's ID?",
         name: "id",
 
         validate: (answer) => {
@@ -68,7 +85,7 @@ function createManager() {
       },
       {
         type: "input",
-        message: "What is your manager's office room number?",
+        message: "What is the manager's office room number?",
         name: "officeNumber",
 
         validate: (answer) => {
@@ -80,50 +97,18 @@ function createManager() {
       },
     ])
     .then((answer1) => {
-      // Process the response by instatiating a new object in the Manager class
-
+      
       console.log(answer1);
-    
-      const newManager = new Manager(answer1.name, answer1.role, answer1.email, answer1.id, answer1.officeNumber)
-
+      // processing the response by instatiating a new object in the Manager class 
+      const newManager = new Manager(answer1.name, answer1.email, answer1.id, answer1.officeNumber)
+      // pushing the new object into an array which will be called to make render HTML file
       teamMembers.push(newManager)
-
-      if (answer1.role === "Manager") {
-        createTeam();
-      } else if (answer1.role === "Engineer") {
-        createEngineer();
-      } else if (answer1.role === "Intern") {
-        createIntern();
-      }
+      createTeam();
+  
     });
 }
 
-// This function starts team creation.
-function createTeam() {
-  inquirer
-    .prompt([
-      // Ask which type of team member should be created with a list of choices
-      {
-        type: "list",
-        message: "Which type of team member do you want to create?: ",
-        name: "role",
-        choices: ["Engineer", "Intern", "Done"],
-      },
-    ])
-    .then((userChoice) => {
-      // Based on which choice they make, call the correct function to ask more questions.
-      // If no choice is made, then go to the rendering function.
-      if (userChoice.role === "Engineer") {
-        createEngineer();
-      } else if (userChoice.role === "Intern") {
-        createIntern();
-      } else if(userChoice.role === "Done"){
-        renderHtmlPage()
-      }
-    });
-}
-
-// This function starts team creation.
+// this function starts team creation.
 function createEngineer() {
   inquirer
     .prompt([
@@ -179,7 +164,7 @@ function createEngineer() {
     .then((answer2) => {
       
       console.log(answer2);
-      
+  
       const newEngineer = new Engineer(answer2.name, answer2.email, answer2.id, answer2.github);
 
       teamMembers.push(newEngineer);
@@ -192,12 +177,11 @@ function createEngineer() {
 function createIntern() {
   inquirer
     .prompt([
-      // STUDENT:  Intern questions
       {
         type: "input",
         message: "What is your intern's name?",
         name: "name",
-        // Note how the validate function works
+        
         validate: (answer) => {
           if (answer !== "") {
             return true;
@@ -245,8 +229,11 @@ function createIntern() {
     .then((answer3) => {
       
       console.log(answer3);
-      const newIntern = new Intern(answer3.name, answer3.email, answer3.id, answer3.school)
-      teamMembers.push(newIntern)
+
+      const newIntern = new Intern(answer3.name, answer3.email, answer3.id, answer3.school);
+
+      teamMembers.push(newIntern);
+
       createTeam();
 
     });
@@ -256,7 +243,9 @@ function createIntern() {
 // and pass INTO it the teamMembers area; from there, write the HTML returned back to a file
 // in a directory called output.
 function renderHtmlPage() {
+
   const html = render(teamMembers);
+
   fs.writeFile("output/index.html", html, error =>{
     if (error){
       return Error
@@ -264,11 +253,8 @@ function renderHtmlPage() {
       return console.log("Success! 'index.html' file generated in folder 'output'")
     }
   })
+
 }
 
-// starter function.
-function startMenu() {
-  createManager();
-}
-
-startMenu();
+// This is our starter function.
+createTeam();
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,8 +1,8 @@
 const Employee = require("./Employee");
 
 class Manager extends Employee {
-  constructor(name, role, email, id, officeNumber) {
-    super(name, role, email, id)
+  constructor(name, email, id, officeNumber) {
+    super(name, "Manager", email, id)
     this.officeNumber = officeNumber;
   }
 
